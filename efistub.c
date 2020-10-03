022e88f13f38a2c496a57d0191f9bcccd2af1d9d
Kakashiiiiy <benedict.schlueter@rub.de>
2020-10-03 12:07:04 +0200

    * Fixed Freezing at startup on my XPS9360

--- efistub.c
+++ efistub.c
@@ -35,11 +35,17 @@ EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTabl
    	err = uefi_call_wrapper(BS->LoadImage, 6, FALSE, ImageHandle, path, NULL, 0, &image);
 
    	err = uefi_call_wrapper(BS->OpenProtocol, 6, image, &LoadedImageProtocol, &loaded_image, ImageHandle, NULL, EFI_OPEN_PROTOCOL_GET_PROTOCOL);
-
-   	loaded_image->LoadOptions = entry->options;
+    
+    loaded_image->LoadOptions = entry->options;
    	loaded_image->LoadOptionsSize = (StrLen(loaded_image->LoadOptions)+1) * sizeof(CHAR16);
 
    	err = uefi_call_wrapper(BS->StartImage, 3, image, NULL, NULL);
-
+    if (EFI_ERROR(err)) {
+      Print(L"Error getting LoadedImageProtocol handle: %r", err);
+      uefi_call_wrapper(BS->Stall, 1, 3 * 1000 * 1000);
+    }
+    uefi_call_wrapper(BS->UnloadImage, 1, image);
+    FreePool(entry);
+    FreePool(path);
    	return err;
 }
